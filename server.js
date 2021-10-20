dbb3b58489f7a4ce3cb556c68963db61120d0c10
Sophie Deskin <sophiedeskin@gmail.com>
2021-10-20 13:16:20 -0700

    uploading service worker file and manifest

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI , {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true
 });
 
 // routes
